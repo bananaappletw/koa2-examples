d97258ed4b4c52f5b092f0fefc173533de83ccb9
bananaappletw <bananaappletw@gmail.com>
2017-02-23 12:19:51 +0800

    Add blog without test

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ lint:
 	@./node_modules/.bin/semistandard --env mocha
 
 test:
-	@NODE_ENV=test node ./node_modules/.bin/_mocha \
-		--compilers babel-core/register \
+	@NODE_ENV=test ./node_modules/.bin/mocha \
+		--compilers js:babel-core/register \
 		--require babel-polyfill \
 		--harmony \
 		--reporter spec \
---
+++ blog/app.js
@@ -0,0 +1,84 @@
+
+/**
+ * Module dependencies.
+ */
+
+import path from 'path';
+import logger from 'koa-logger';
+import Router from 'koa-router';
+import views from 'koa-views';
+import bodyParser from 'koa-bodyparser';
+import Koa from 'koa';
+const app = new Koa();
+const router = new Router();
+
+// "database"
+
+var posts = [];
+
+// middleware
+
+app.use(bodyParser());
+app.use(logger());
+
+// route middleware
+
+router.get('/', list);
+router.get('/post/new', add);
+router.get('/post/:id', show);
+router.post('/post', create);
+
+app
+  .use(views(path.join(__dirname, 'views'), {
+    extention: 'html',
+    map: {
+      html: 'swig'
+    }
+  }))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+/**
+ * Post listing.
+ */
+
+async function list (ctx, next) {
+  await ctx.render('list', {
+    posts: posts
+  });
+}
+
+/**
+ * Show creation form.
+ */
+
+async function add (ctx) {
+  await ctx.render('new');
+}
+
+/**
+ * Show post :id.
+ */
+
+async function show (ctx, next) {
+  var post = posts[ctx.params.id];
+  if (!post) this.throw(404, 'invalid post id');
+  await ctx.render('show', { post: post });
+}
+
+/**
+ * Create a post.
+ */
+
+async function create (ctx) {
+  var post = await ctx.request.body;
+  var id = posts.push(post) - 1;
+  post.created_at = new Date();
+  post.id = id;
+  ctx.redirect('/');
+}
+
+// listen
+
+if (!module.parent) app.listen(3000);
+export default app;
